6161c044851762f805958f01adc3b8be15278e6a
Yuhei Nishi <abc-gtgtg.21q.abds@ezweb.ne.jp>
2020-04-21 10:08:43 +0900

    Create group

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
- 
- devise_for :users
-  root "messages#index"
+
+  devise_for :users
+  root 'groups#index'
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update]
 end
