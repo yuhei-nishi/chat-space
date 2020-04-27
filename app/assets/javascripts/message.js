5cc33f5e47c19ccf0b592708df9ae17c74546c15
Yuhei Nishi <abc-gtgtg.21q.abds@ezweb.ne.jp>
2020-04-27 09:25:18 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -69,7 +69,7 @@ $(function(){
       url: "api/messages", 
       type: 'get', 
       dataType: 'json',
-      data: {last_id: last_message_id} 
+      data: {id: last_message_id} 
     })
     .done(function (messages) {
       if (messages.length !== 0) { 
